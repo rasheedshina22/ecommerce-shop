8115a543c0deba3a5a0d199ff3369a6062732528
shina <rasheedshinaopeyemi@gmail.com>
2021-04-16 18:35:39 +0100

    added the product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Switch>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 

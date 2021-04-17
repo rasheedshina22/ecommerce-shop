8c5050dbf64bed2cdc0e8d54e7f626666e3ac85f
shina <rasheedshinaopeyemi@gmail.com>
2021-04-17 04:36:14 +0100

    added database connection

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((product) => product._id === match.params.id);
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [match]);
   return (
     <>
       <Link className="btn btn-light mt-3" to="/">
         Go Back
       </Link>
-      <Row>
+      <Row className="mt-3">
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={3}>
           <ListGroup variant="flush">
             <ListGroup.Item>
-              <h2>{product.name}</h2>
+              <h2 style={{ fontSize: '15px' }}>{product.name}</h2>
             </ListGroup.Item>
             <ListGroup.Item>
               <Rating

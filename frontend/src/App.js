aef40c8b587a9e5e92d2d16bd1a351d8d71c2a1c
shina <rasheedshinaopeyemi@gmail.com>
2021-04-17 09:13:19 +0100

    addded the cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Switch>
             <Route exact path="/" component={HomeScreen} />
             <Route path="/product/:id" component={ProductScreen} />
+            {/* this means the id is optional when we go to /cart/id and /cart we would see thesame thing */}
+            <Route path="/cart/:id?" component={CartScreen} />
           </Switch>
         </Container>
       </main>

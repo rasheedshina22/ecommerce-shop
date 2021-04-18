c85bf54e0ee72d200561a15e7cdaf317e7968391
shina <rasheedshinaopeyemi@gmail.com>
2021-04-18 22:46:08 +0100

    implemented user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc authenticate user and get token
 // @route POST /api/users/login
@@ -10,11 +11,11 @@ const loginUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
     res.json({
-      id: user._id,
+      _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
@@ -22,4 +23,56 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { loginUser };
+// @desc register a user and get token
+// @route POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exist');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    re.status(404);
+    throw new Error('user not found');
+  }
+
+  res.send('success');
+});
+
+export { loginUser, getUserProfile, registerUser };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,15 @@
 import express from 'express';
 const router = express.Router();
 
-import { loginUser } from '../controllers/userController.js';
+import {
+  loginUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
+import { authenticate } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser);
 router.post('/login', loginUser);
+router.route('/profile').get(authenticate, getUserProfile);
 
 export default router;
